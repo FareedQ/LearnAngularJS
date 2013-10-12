71093bc14e086ef9f84d61516e8112faf9e904b4
Fareed Quraishi <quraishifareed@gmail.com>
2013-10-11 20:09:57 -0400

    Introduced Filters
    Converted the function into a filter.  Adjusted how it was applied in
    the HMTL of the code as well as used the data from the factory class to
    show it was capable of being used.

--- testSpace/main.js
+++ testSpace/main.js
@@ -3,14 +3,16 @@ myApp.factory('Data', function() {
 	return {message:"I'm data from a service"}
 });
 
+myApp.filter('reverse', function(Data){
+	return function (input) {
+		return input.split("").reverse().join("") + " " + Data.message;
+	}
+})
+
 function firstCtrl($scope, Data) {
 	$scope.data = Data;
 }
 
 function secondCtrl($scope, Data) {
 	$scope.data = Data;
-
-	$scope.reversedMessage = function(input){
-		return input.split("").reverse().join("");
-	}
 }
