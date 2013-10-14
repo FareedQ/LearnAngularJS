add6f66a71be371a34da4cb5f2f8aee5b88356a9
Fareed Quraishi <quraishifareed@gmail.com>
2013-10-14 13:10:40 -0400

    Learn ng-repeat and filtering
    This lesson added the ng-repeat capabilities and started using already
    existing filters.

--- testSpace/main.js
+++ testSpace/main.js
@@ -1,13 +1,29 @@
 var myApp = angular.module('myApp', []);
 myApp.factory('Data', function() {
-	return {message:"I'm data from a service"}
+	var Data = {}
+	Data.message = "I'm data from a service";
+	Data.superHeros = [
+		{
+			secretIdentity: "Bruce Bannar",
+			hero: "Hulk"
+		},
+		{
+			secretIdentity: "Bruce Wayne",
+			hero: "Batman"
+		},
+		{
+			secretIdentity: "Clark Kent",
+			hero: "Superman"
+		}
+	];
+	return Data;
 });
 
 myApp.filter('reverse', function(Data){
 	return function (input) {
-		return input.split("").reverse().join("") + " " + Data.message;
+		return input.split("").reverse().join("");
 	}
-})
+});
 
 function firstCtrl($scope, Data) {
 	$scope.data = Data;
@@ -15,4 +31,8 @@ function firstCtrl($scope, Data) {
 
 function secondCtrl($scope, Data) {
 	$scope.data = Data;
+}
+
+function thirdCtrl($scope, Data) {
+	$scope.superHeros = Data.superHeros;
 }
